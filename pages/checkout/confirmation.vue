a803128a78214acbabe51e57f33db828b1f4b107
Hannes Kuhl <hanneskuhl@live.de>
2020-10-24 19:44:02 +0200

    added transaction id button

--- pages/checkout/confirmation.vue
+++ pages/checkout/confirmation.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
     <div class="card__container">
-      <h2>
-        Thank you, {{ delivery.firstName }}, for supporting world peace
-      </h2>
+      <h2>Thank you, {{ delivery.firstName }}, for supporting world peace</h2>
+      <h4>
+        Your order ID is : <code>{{ transactionId }}</code>
+      </h4>
     </div>
     <div class="is-h-centered">
       <nuxt-link to="/checkout/personalization">
@@ -17,28 +18,20 @@
 export default {
   data() {
     return {
-      delivery: {
-        firstName: "",
-        lastName: "",
-        street: "",
-        zipCode: "",
-        city: "",
-        country: "",
-      },
+      transactionId: null
     };
   },
   mounted() {
     try {
-      const deliveryData = JSON.parse(localStorage.getItem("delivery"));
-      if (deliveryData) {
-        this.delivery = deliveryData;
+      const transactionId = JSON.parse(localStorage.getItem('summary'));
+      if (transactionId) {
+        this.transactionId = transactionId;
       }
     } catch (e) {
-      console.error("error getting data from localstorage", e);
+      console.error('error getting data from localstorage', e);
     }
-  },
+  }
 };
 </script>
 
-<style>
-</style>
+<style></style>
